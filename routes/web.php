0ff81a9ca0f097ded2c970753d7d08aad6197561
kirigaya211 <kirigayarz@gmail.com>
2024-07-18 22:41:49 +0800

    updating

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function(){
     return 'Home page';
-});
+})->name('home.index');
+
+Route::get('/contact', function(){
+    return 'Contact';
+})->name('contact.index');
+
+Route::get('/posts/{id}', function($id){
+    return 'Blog post ' .$id;
+})->name('posts.show');
+
+
+Route::get('/recent-posts/{days_ago?}', function($daysAgo=20){
+    return 'Posts from ' . $daysAgo . ' days ago';
+})->name('post.recent.index');
