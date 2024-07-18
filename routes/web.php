84b4f626ceb2a9293568eb995547c78c61656bf2
kirigaya211 <kirigayarz@gmail.com>
2024-07-19 06:06:45 +0800

    morning changes for practice

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,57 @@
 
 use Illuminate\Support\Facades\Route;
 
+$posts = [
+    1 => [
+        'title' => 'Intro to Laravel',
+        'content' => 'This is a short intro to Laravel',
+        'is_new'=>true,
+        'has_comments' =>true,
+    ],
+    2 => [
+        'title' => 'Intro to PHP',
+        'content' => 'This is a short intro to PHP',
+        'is_new'=>false,
+       
+    ],
+    3 => [
+        'title' => 'Intro to Golang',
+        'content' => 'This is a short intro to Golang',
+        'is_new'=>false,
+       
+    ]
+];
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
 
-Route::get('/', function(){
-    return 'Home page';
-})->name('home.index');
+// Route::get('/', function(){
+//     return view('home.index', []);
+// })->name('home.index');
 
-Route::get('/contact', function(){
-    return 'Contact';
-})->name('contact.index');
+// Route::get('/contact', function(){
+//     return view('home.contact');
+// })->name('contact.index');
 
-Route::get('/posts/{id}', function($id){
-    return 'Blog post ' .$id;
-})->name('posts.show');
+Route::view('/', 'home.index')->name('home.index');
+Route::view('/contact', 'home.contact')->name('home.contact');
+
+Route::get('/posts', function() use ($posts){
+    // compact($posts)
+    return view('posts.index',['posts'=>$posts]);
+});
+
+Route::get('/posts/{id}', function($id) use($posts){
+    abort_if(!isset($posts[$id]), 404);
+    return view('posts.show', ['post'=>$posts[$id]]);
+})->where([
+    'id'=>'[0-9]+'
+])->name('posts.show');
 
 
 Route::get('/recent-posts/{days_ago?}', function($daysAgo=20){
     return 'Posts from ' . $daysAgo . ' days ago';
 })->name('post.recent.index');
+
+
